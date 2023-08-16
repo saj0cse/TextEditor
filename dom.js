e9349fcca02e9235bece4c6aedec5f2cc2848e63
Shakil Ahammed <shakilahammedjoy022@gmail.com>
2023-08-16 16:09:06 +0600

    first commit

---
+++ dom.js
@@ -0,0 +1,13 @@
+
+// blod 
+document.getElementById('blod').addEventListener('click', function(){
+    document.getElementById('text-input').style.fontWeight = "900"; 
+})
+// italic 
+document.getElementById('italic').addEventListener('click', function(){
+    document.getElementById('text-input').style.fontStyle  = "italic"; 
+})
+// underline 
+document.getElementById('underline').addEventListener('click', function(){
+    document.getElementById('text-input').style.textDecoration = "underline"; 
+})
